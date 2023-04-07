7a9441514005364c7d0e5151817e46609a6b4bfe
Ishwaryasureshbabu <126793772+Ishwaryasureshbabu@users.noreply.github.com>
2023-04-07 20:15:22 +0530

    Add files via upload

---
+++ chat application/src/js/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ChatApp from "./App";
+import App from "./App.1"
+ReactDOM.render(<ChatApp />, document.getElementById("root"));
